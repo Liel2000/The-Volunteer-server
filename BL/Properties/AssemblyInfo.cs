4be0f99599e711e860d21f0e2e710655104b1f7e
ליאל בוקובזה <florinb1972@gmail.com>
2022-06-27 22:59:33 +0300

    change directory names

--- BL/Properties/AssemblyInfo.cs
+++ BL/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
+[assembly: AssemblyTitle("BL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
